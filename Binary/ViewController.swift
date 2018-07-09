e60a91a360aee1dd25ed7464997d4503a2f84cb6
Cameron Bardell <cambardell3@gmail.com>
2018-07-09 15:51:36 -0400

    Initial Commit

---
+++ Binary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Binary
+//
+//  Created by Cameron Bardell on 2018-07-09.
+//  Copyright © 2018 101. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
